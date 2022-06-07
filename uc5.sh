db2b321200426d5c99176042d59c4bcd785dc27c
munirathnap6360 <ratnapapanna53752@gmail.com>
2022-06-07 22:52:49 +0530

    uc5

---
+++ uc5.sh
@@ -0,0 +1,23 @@
+echo "-----------Welcome to sorting problems-------------"
+#uc1
+read -p "Enter a value: " a
+read -p "Enter b value: " b
+read -p "Enter c value: " c
+
+echo "UC1: Values of a=$a, b=$b and c=$c"
+
+#uc2
+uc2=$((a+b*c))
+echo "UC2: is $uc2"
+
+#uc3
+uc3=$((a*b+c))
+echo "UC3: is $uc3"
+
+#uc4
+uc4=$((c+a/b))
+echo "UC4: is $uc4"
+
+#uc5
+uc5=$((a%b+c))
+echo "UC5: is $uc5"
