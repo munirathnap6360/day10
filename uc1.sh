308f13a4b0b28c4bd05d1ec6963edd3bed397e9f
munirathnap6360 <ratnapapanna53752@gmail.com>
2022-06-07 22:17:12 +0530

    three input

---
+++ uc1.sh
@@ -0,0 +1,7 @@
+cho "-----------Welcome to sorting problems-------------"
+#uc1
+read -p "Enter a value: " a
+read -p "Enter b value: " b
+read -p "Enter c value: " c
+
+echo "UC1: Values of a=$a, b=$b and c=$c"
